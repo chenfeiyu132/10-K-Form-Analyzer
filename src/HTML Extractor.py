27dde4cebceacc46b26f1c908373cb1b7b777b47
chenfeiyu132 <chenfeiyu132@gmail.com>
2019-12-31 21:25:12 +0800

    Changed approach to cut out item 8 from deletion to completely creating a new soup object for export
    Optimized find_root_parent to more accurately detect parent tag

--- src/HTML Extractor.py
+++ src/HTML Extractor.py
@@ -1,17 +1,13 @@
 from bs4 import BeautifulSoup as bs
 import re
+import os
+import copy
 
 # Process outline:
     # locate the start of item_8
     # locate the start of item_9
     # extract/delete anything before start of item_8 and after start of item_9
     # output new html after extraction
-
-page = open('/Users/Ju1y/Documents/GIES Research Project/10-K/2010Q1/2098-ACME UNITED CORP-10-K-2010-03-09.html')
-soup = bs(page.read(), "lxml")
-
-item_8_start = ''
-item_9_start = ''
 
 
 def delete_section(beginning, end):  # deletes section based on beginning html tag and end html tag
@@ -31,56 +27,73 @@ def delete_section(beginning, end):  # deletes section based on beginning html t
                 break
 
 
-def find_root_parent(element, grandparent):  # finds the top most parent for certain element under body
-    while element.parent and element.parent.name != grandparent:
-        element = element.parent
-    return element
+def find_root_parent(tag_1, reference):  # finds the top most parent for certain element under body
+    if not tag_1:
+        return tag_1
+    while tag_1.parent and reference not in tag_1.parent.descendants:
+        tag_1 = tag_1.parent
+    return tag_1
 
 
-def locate_item(regex, grandparent):
+def locate_item(regex, soup):
 
     locators = soup.findAll(text=re.compile(regex, re.IGNORECASE))
     if locators:
         for tag in locators:
-            if tag.parent.name == 'b':
+            bold = tag.find_parent('b')
+            font = tag.find_parent('font', style=re.compile(r'FONT-WEIGHT: bold'))
+            if bold:
                 print('tag_name: ', 'b')  # debugging purposes
-                return find_root_parent(tag, grandparent)
-            elif tag.parent.name == 'font' and tag.parent.has_attr('style') \
-                    and 'FONT-WEIGHT: bold' in tag.parent['style']:
-                        print('tag_name: ', 'font')  # debuging purposes
-                        return find_root_parent(tag, grandparent)
+                return bold
+            elif font:
+                print('tag_name: ', 'font')  # debuging purposes
+                return font
     print('item not found')
     return None
 
 
-def locate_next_item(tag, regex, grandparent):
-    locators = tag.find_all_next(text=re.compile(regex, re.IGNORECASE))
-    if locators:
-        for t in locators:
-            if tag.find('b') and t.parent.name == 'b':
-                return find_root_parent(t, grandparent)
-            elif t.parent.name == 'font' and t.parent.has_attr('style') and 'FONT-WEIGHT: bold' in t.parent['style']:
-                return find_root_parent(t, grandparent)
-    print('next item not found')
-    return None
+def append_tags(begin, end, soup):
+    if not begin:
+        return soup
+    for tag in begin.find_next_siblings():
+        if tag != end:
+            soup.append(tag)
+        else:
+            break
+    return soup
 
 
-item_8_start = locate_item(r'FINANCIAL\s*STATEMENTS\s*AND\s*SUPPLEMENTARY\s*DATA', 'text')
-item_9_start = locate_next_item(item_8_start, r'CHANGES\s*IN\s*AND\s*DISAGREEMENTS\s*WITH\s*'
-                                r'ACCOUNTANTS\s*ON\s*ACCOUNTING\s*AND\s*FINANCIAL\s*'
-                                r'DISCLOSURE', 'text')
+def process_html(path, output_folder_path):
+    page = open(path)
+    soup = bs(page.read(), "lxml")
+    file_name = os.path.basename(path)
+
+    item_8_start = locate_item(r'FINANCIAL\s*STATEMENTS\s*AND\s*SUPPLEMENTARY\s*DATA', soup)
+    item_9_start = locate_item(r'CHANGES\s*IN\s*AND\s*DISAGREEMENTS\s*WITH\s*'
+                                    r'ACCOUNTANTS\s*ON\s*ACCOUNTING\s*AND\s*FINANCIAL\s*'
+                                    r'DISCLOSURE', soup)
+    item_8_parent = find_root_parent(item_8_start, item_9_start)
+    item_9_parent = find_root_parent(item_9_start, item_8_start)
+    new_soup = bs(features='lxml')
+    if item_8_start and item_9_start:
+
+        new_soup = append_tags(item_8_parent.previous_sibling, item_9_parent, new_soup)
+        output_path = output_folder_path + file_name
+        print(output_path)
+        with open(output_path, 'w', encoding='utf-8') as file:  # output processed soup into html
+            file.write(str(new_soup))
 
 
-delete_section('', item_8_start)  # deletes everything up to item 8
-delete_section(item_9_start, '')  # deletes everything after start of item 9
-item_9_start.extract()  # deletes start of item 9
-
-item_document = locate_item(r'FINANCIAL\s*STATEMENTS\s*AND\s*SUPPLEMENTARY\s*DATA', 'sec-document')
-
-delete_section(item_document, '')  # deletes every other document besides the one which item 8 is located in
-
-with open('test_output_2.html', 'w', encoding='utf-8') as file:  # output processed soup into html
-    file.write(str(soup))
 
 
+directory = '/Users/Ju1y/Documents/GIES Research Project/10-K/2010Q1/'
+output_folder = '/Users/Ju1y/Documents/GIES Research Project/Item8/'
 
+for filename in os.listdir(directory):
+    if filename.endswith('.html'):
+        print(filename)
+        process_html(directory+filename, output_folder)
+    # elif filename.endswith('.txt'):
+    #     pre, ext = os.path.splitext(directory+'/'+filename)
+    #     os.rename(directory+'/'+filename, pre + '.html')
+    #     process_html(pre + '.html', output_folder+'/'+os.path.splitext(filename)[0])
