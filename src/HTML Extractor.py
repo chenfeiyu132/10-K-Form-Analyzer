8dd2fbea62b8dc3f809665dff08cf06b9006836d
chenfeiyu132 <chenfeiyu132@gmail.com>
2020-01-01 20:57:10 +0800

    Optimized regex expression for item 9 to increase search accuracy

--- src/HTML Extractor.py
+++ src/HTML Extractor.py
@@ -52,9 +52,9 @@ def process_html(path, output_folder_path):
     file_name = os.path.basename(path)
 
     item_8_start = locate_item(r'FINANCIAL\s*STATEMENTS\s*AND\s*SUPPLEMENTARY\s*DATA', soup)
-    item_9_start = locate_item(r'CHANGES\s*IN\s*AND\s*DISAGREEMENTS\s*WITH\s*'
-                                    r'ACCOUNTANTS\s*ON\s*ACCOUNTING\s*AND\s*FINANCIAL\s*'
-                                    r'DISCLOSURE', soup)
+    item_9_start = locate_item(r'(ITEM)?.*?9?.*?(CHANGES)?.?\s*(IN)?.?\s*(AND)?.?\s*(DISAGREEMENTS)?.?\s*(WITH)?.?\s*'
+                                    r'(ACCOUNTANTS)?.?\s*(ON)?.?\s*(ACCOUNTING)?.?\s*(AND)?.?\s*(FINANCIAL)?.?\s*'
+                                    r'(DISCLOSURE)?.?', soup)
     item_8_parent = find_root_parent(item_8_start, item_9_start)
     item_9_parent = find_root_parent(item_9_start, item_8_start)
     new_soup = bs(features='lxml')
@@ -62,7 +62,7 @@ def process_html(path, output_folder_path):
 
         new_soup = append_tags(item_8_parent.previous_sibling, item_9_parent, new_soup)
         output_path = output_folder_path + file_name
-        print(output_path)
+        #print(output_path)
         with open(output_path, 'w', encoding='utf-8') as file:  # output processed soup into html
             file.write(str(new_soup))
 
@@ -70,11 +70,16 @@ def process_html(path, output_folder_path):
 directory = '/Users/Ju1y/Documents/GIES Research Project/10-K/2010Q1/'
 output_folder = '/Users/Ju1y/Documents/GIES Research Project/Item8/'
 
+count = 0
+
+
 for filename in os.listdir(directory):
     if filename.endswith('.html'):
         print(filename)
         process_html(directory+filename, output_folder)
-    # elif filename.endswith('.txt'):
-    #     pre, ext = os.path.splitext(directory+'/'+filename)
-    #     os.rename(directory+'/'+filename, pre + '.html')
-    #     process_html(pre + '.html', output_folder+'/'+os.path.splitext(filename)[0])
+    elif filename.endswith('.txt') and count < 20:
+        print(filename)
+        pre, ext = os.path.splitext(directory+filename)
+        os.rename(directory+filename, pre + '.html')
+        process_html(pre + '.html', output_folder)
+        count += 1
