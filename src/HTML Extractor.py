46515499102b084e97223c0ecbdde6453467b66a
chenfeiyu132 <chenfeiyu132@gmail.com>
2020-01-04 12:40:35 +0800

    Changed approach of find_root_parent to deal with edge binding cases

--- src/HTML Extractor.py
+++ src/HTML Extractor.py
@@ -6,7 +6,7 @@ import copy
 # Process outline:
     # locate the start of item_8
     # locate the start of item_9
-    # extract/delete anything before start of item_8 and after start of item_9
+    # copy everything between item_8 and start of item_9 to new html
     # output new html after extraction
 
 
@@ -14,7 +14,7 @@ def find_root_parent(tag_1, reference):  # finds the top most parent for certain
     if not tag_1:
         return tag_1
     while tag_1.parent and reference not in tag_1.parent.descendants:
-        tag_1 = tag_1.parent
+        tag_1.parent.unwrap()
     return tag_1
 
 
@@ -24,7 +24,7 @@ def locate_item(regex, soup):
     if locators:
         for tag in locators:
             bold = tag.find_parent('b')
-            font = tag.find_parent('font', style=re.compile(r'FONT-WEIGHT: bold'))
+            font = tag.find_parent(style=re.compile(r'FONT-WEIGHT:\s*bold', re.IGNORECASE))
             if bold:
                 print('tag_name: ', 'b')  # debugging purposes
                 return bold
@@ -36,8 +36,6 @@ def locate_item(regex, soup):
 
 
 def append_tags(begin, end, soup):
-    if not begin:
-        return soup
     for tag in begin.find_next_siblings():
         if tag != end:
             soup.append(tag)
@@ -50,16 +48,15 @@ def process_html(path, output_folder_path):
     page = open(path)
     soup = bs(page.read(), "lxml")
     file_name = os.path.basename(path)
-
-    item_8_start = locate_item(r'FINANCIAL\s*STATEMENTS\s*AND\s*SUPPLEMENTARY\s*DATA', soup)
-    item_9_start = locate_item(r'(ITEM)?.*?9?.*?(CHANGES)?.?\s*(IN)?.?\s*(AND)?.?\s*(DISAGREEMENTS)?.?\s*(WITH)?.?\s*'
-                                    r'(ACCOUNTANTS)?.?\s*(ON)?.?\s*(ACCOUNTING)?.?\s*(AND)?.?\s*(FINANCIAL)?.?\s*'
-                                    r'(DISCLOSURE)?.?', soup)
+    item_8_start = locate_item(r'((ITEM)\s*8)|FINANCIAL\s*STATEMENTS\s*AND\s*SUPPLEMENTARY?\s*DATA', soup)
+    item_9_start = locate_item(r'((ITEM)\s*9)|(CHANGES).?\s*(IN).?\s*(AND).?\s*(DISAGREEMENTS).?\s*(WITH).?\s*'
+                                    r'(ACCOUNTANTS).?\s*(ON).?\s*(ACCOUNTING).?\s*(AND).?\s*(FINANCIAL).?\s*'
+                                    r'(DISCLOSURE).?', soup)
     item_8_parent = find_root_parent(item_8_start, item_9_start)
+    print(item_8_start)
     item_9_parent = find_root_parent(item_9_start, item_8_start)
     new_soup = bs(features='lxml')
     if item_8_start and item_9_start:
-
         new_soup = append_tags(item_8_parent.previous_sibling, item_9_parent, new_soup)
         output_path = output_folder_path + file_name
         #print(output_path)
@@ -70,16 +67,16 @@ def process_html(path, output_folder_path):
 directory = '/Users/Ju1y/Documents/GIES Research Project/10-K/2010Q1/'
 output_folder = '/Users/Ju1y/Documents/GIES Research Project/Item8/'
 
-count = 0
+#count = 0
 
-
-for filename in os.listdir(directory):
-    if filename.endswith('.html'):
-        print(filename)
-        process_html(directory+filename, output_folder)
-    elif filename.endswith('.txt') and count < 20:
-        print(filename)
-        pre, ext = os.path.splitext(directory+filename)
-        os.rename(directory+filename, pre + '.html')
-        process_html(pre + '.html', output_folder)
-        count += 1
+process_html(directory+'3202-ALASKA AIRLINES INC-10-K-2010-02-24.html', output_folder)
+# for filename in os.listdir(directory):
+#     if filename.endswith('.html'):
+#         print(filename)
+#         process_html(directory+filename, output_folder)
+    # elif filename.endswith('.txt') and count < 20:
+    #     print(filename)
+    #     pre, ext = os.path.splitext(directory+filename)
+    #     os.rename(directory+filename, pre + '.html')
+    #     process_html(pre + '.html', output_folder)
+    #     count += 1
