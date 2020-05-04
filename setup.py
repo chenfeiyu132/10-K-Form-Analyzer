06e4c8913bd3088726447e869c941128a4ee52b9
chenfeiyu132 <chenfeiyu132@gmail.com>
2020-05-03 21:18:53 -0700

    added requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='10-K-Form-Analyzer',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Ju1y',
+    author_email='',
+    description=''
+)
