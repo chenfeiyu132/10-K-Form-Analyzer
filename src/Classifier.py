16cdd35b7bebd4f5cd5dec463769030f5004ae1e
chenfeiyu132 <chenfeiyu132@gmail.com>
2020-05-11 19:34:56 -0700

    now tuning alpha value

--- src/Classifier.py
+++ src/Classifier.py
@@ -151,10 +151,10 @@ mnb_pipeline = Pipeline([
 ])
 # different parameter settings to test out
 grid_params = {
-    'mnb__alpha': [0.7],
+    'mnb__alpha': np.linspace(0, 1, 0.1),
     'mnb__fit_prior': [True],
     'tfidf_pipeline__ngram_range': [(1,2)],
-    'tfidf_pipeline__max_df': np.linspace(0.1, 1, 10),
+    'tfidf_pipeline__max_df': [.2],
     'tfidf_pipeline__binary': [True],
     'tfidf_pipeline__norm': [None],
 }
