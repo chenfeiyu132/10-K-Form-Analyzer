1f4da0cade5d97e116c677d6e86e1781ed130b48
chenfeiyu132 <chenfeiyu132@gmail.com>
2020-05-22 19:21:51 -0700

    added function to generate confusion matrix from test set

--- src/Classifier.py
+++ src/Classifier.py
@@ -5,6 +5,8 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import LinearSVC
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
 
 from nltk.stem import WordNetLemmatizer #for ignoring common words
 # from nltk.tokenize import PunktSentenceTokenizer, ToktokTokenizer
@@ -14,6 +16,7 @@ import numpy as np
 import os
 import csv
 import sys
+import matplotlib.pylab as plt
 
 en_stop = text.ENGLISH_STOP_WORDS
 stemmer = WordNetLemmatizer()
@@ -127,6 +130,57 @@ def top_terms(classifier, feature_names, top_features=10):
     # plt.show()
 
 
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.0
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
+
 path_to_csv = 'label_reference.csv' if sys.platform == 'darwin' else 'src/label_reference.csv'
 df_csv = pd.read_csv(open(path_to_csv, 'rb'))
 
@@ -209,11 +263,29 @@ grid_params = {
     'tfidf_pipeline__binary': [True],
     'tfidf_pipeline__norm': [None],
 }
+
+full_text_train, full_text_test, label_train, label_test = train_test_split(df_all_forms['full text'],
+                                                                            df_all_forms['prosecution'],
+                                                                            test_size=1/3, random_state=85)
+
 clf = GridSearchCV(mnb_pipeline, grid_params, cv=5)
-clf.fit(df_all_forms['full text'], df_all_forms['prosecution'])
+clf.fit(full_text_train, label_train)
 
 print('Best Score: ', clf.best_score_)
 print('Best Params: ', clf.best_params_)
+
+# generate confusion matrix
+label_pred = clf.best_estimator_.predict(full_text_test)
+
+plot_confusion_matrix(label_test, label_pred,
+                      classes=['NonProsecution', 'Prosecution'],
+                      title='Confusion matrix, without normalization')
+plt.savefig('unnormalized graph.png')
+plot_confusion_matrix(label_test, label_pred,
+                      classes=['NonProsecution', 'Prosecution'], normalize=True,
+                      title='Normalized confusion matrix')
+plt.savefig('normalized graph.png')
+
 
 # NB_optimal = MultinomialNB(alpha=.1, fit_prior=True)
 # X_train = tfidf.fit_transform(df_all_forms['full text'])
