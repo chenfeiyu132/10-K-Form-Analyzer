feddd2746a7b1b79ac1509707775c26b7d6a250b
chenfeiyu132 <chenfeiyu132@gmail.com>
2020-07-03 21:40:19 -0700

    switching mechanism for checking for null values

--- src/Classifier.py
+++ src/Classifier.py
@@ -316,7 +316,7 @@ for ind in df_csv.index:
             before = locate_prior_files(directory, str(actual_year), quarter, str(cik), num_years_prior)
             for index_prior in range(len(before)):
                 head, tail = os.path.split(before[index_prior])
-                if df_csv['settle_m{0}'.format(index_prior+1)][ind].isnull():
+                if pd.isna(df_csv['settle_m{0}'.format(index_prior+1)][ind]):
                     continue
                 elif int(df_csv['settle_m{0}'.format(index_prior+1)][ind]) != '0':
                     shutil.copyfile(before[index_prior], directory+'Disclosure/'+tail)
